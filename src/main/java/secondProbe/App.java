f43e3b1e0d9ffcf63f9289e8ea28ef04245a5e12
koczkaAD <winifred9103@gmail.com>
2018-01-27 13:53:39 +0100

    first commit

---
+++ src/main/java/secondProbe/App.java
@@ -0,0 +1,13 @@
+package secondProbe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
